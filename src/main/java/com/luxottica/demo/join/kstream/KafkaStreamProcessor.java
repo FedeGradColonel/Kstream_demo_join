d908f6ea5edbc52f2bf2067ba1e92bf682ace069
federicogradizzi <federico.gradizzi@bitrock.it>
2024-05-14 15:43:54 +0200

    refactor code

--- src/main/java/com/luxottica/demo/join/kstream/KafkaStreamProcessor.java
+++ src/main/java/com/luxottica/demo/join/kstream/KafkaStreamProcessor.java
@@ -1,4 +1,4 @@
-package com.luxottica.demo.join.service;
+package com.luxottica.demo.join.kstream;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
@@ -6,20 +6,24 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Properties;
 
-@Service
-public class JoinService {
+@Configuration
+public class KafkaStreamProcessor {
 
-    public void doJoin() {
+    @Value("${spring.kafka.streams.bootstrap-servers}")
+    private String boostrapServer;
 
-        Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-stream-join-demo");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+    @Value("${spring.kafka.streams.application-id}")
+    private String applicationId;
+
+    @Bean
+    public KafkaStreams KafkaProcessor() {
+        Properties properties = getProperties();
 
         StreamsBuilder builder = new StreamsBuilder();
 
@@ -31,7 +35,7 @@ public class JoinService {
 
         // Join tra lo stream 'appointment' e la 'practitioner' KTable
         appointmentStream
-                .leftJoin(practitionerTable,
+                .join(practitionerTable,
                         (appointmentValue, practitionerValue) -> {
                             // Qui gestisci il caso in cui non ci sia una corrispondenza in practitioner
                             if (practitionerValue == null) {
@@ -42,10 +46,21 @@ public class JoinService {
                         })
                 .to("appointment-practitioner-join");
 
-        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        KafkaStreams streams = new KafkaStreams(builder.build(), properties);
         streams.start();
 
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+        return streams;
+    }
+
+    public Properties getProperties() {
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServer);
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        return properties;
     }
 
 }
